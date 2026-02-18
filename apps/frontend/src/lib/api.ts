11588f7c11153fd925a66e0adfb0f0f2b478bf9e
Noel De Martin <noel@noeldemartin.com>
2026-02-18 08:59:18 +0100

    Add frontend

---
+++ apps/frontend/src/lib/api.ts
@@ -0,0 +1,6 @@
+import { treaty } from '@elysiajs/eden';
+import type { Api } from '@anima/webserver';
+
+const api: ReturnType<typeof treaty<typeof Api>> = treaty<typeof Api>('localhost:3000');
+
+export default api;
--- apps/webserver/src/index.ts
+++ apps/webserver/src/index.ts
@@ -1,10 +1,13 @@
 import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
 import { node } from '@elysiajs/node';
 
 const PORT = process.env.PORT ?? 3000;
 
-new Elysia({ adapter: node() })
+export const Api = new Elysia({ adapter: node() })
+  .use(cors())
   .get('/', () => 'Hello World')
+  .get('/ping', () => ({ pong: true }))
   .listen(PORT, ({ hostname, port }) => {
     console.log(`🟢 Server running at http://${hostname}:${port}`);
   });
