ac5c6fc73f7eb2664f40fedba9c404ed22b40005
Noel De Martin <noel@noeldemartin.com>
2026-03-15 17:38:53 +0100

    Configure baseUrl

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-    Aerogel({ name: 'Ànima', soukaiBis: true }),
+    Aerogel({ name: 'Ànima', soukaiBis: true, baseUrl: 'https://anima.noeldemartin.com' }),
     Components({
       deep: true,
       dts: 'src/types/components.d.ts',
