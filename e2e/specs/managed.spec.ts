5c9a2b1e88047570c323eac3db8ad699f979c278
Noel De Martin <noel@noeldemartin.com>
2026-09-22 13:20:00 +0200

    Fix E2E

--- e2e/specs/managed.spec.ts
+++ e2e/specs/managed.spec.ts
@@ -15,7 +15,8 @@ test('register', async ({ page }) => {
   await page.getByRole('link', { name: 'Register' }).click();
   await page.getByLabel('Email').fill('test@example.com');
   await page.getByLabel('Username').fill('testing');
-  await page.getByLabel('Password').fill('secret');
+  await page.getByLabel('Password', { exact: true }).fill('secret');
+  await page.getByLabel('Confirm Password').fill('secret');
   await page.getByRole('button', { name: 'Register' }).click();
 
   await expect(page.getByText(`Hello ${podUrl('testing/profile/card#me')}, how can I help you today?`)).toBeVisible();
